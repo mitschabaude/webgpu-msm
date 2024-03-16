6dd57e47ffea2b640c25b7b425ec85763152f797
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-03-16 11:03:15 +0100

    homogenize test scripts

--- src/submission/submission-bls377-test.ts
+++ src/submission/submission-bls377-test.ts
@@ -1,6 +1,6 @@
 import { compute_msm } from "./submission-bls377.js";
 import { curveParams } from "./src/concrete/bls12-377.params.js";
-// just a basic sanity check that the submission code can run and handles a trivial test case
+// sanity check that the submission code can run and handles a basic test case
 
 let point = {
   x: 22751760760637705504878160027698824622458942230958800913144762406971228535105703540544554687017331773376179053030n,
@@ -12,6 +12,7 @@ let scalars = [2n, curveParams.order - 1n];
 // 2*P + (-1)*P should give P again
 let result = await compute_msm([point, point], scalars);
 
+// check result
 if (result.x !== point.x || result.y !== point.y) {
   throw Error("failed");
 }
